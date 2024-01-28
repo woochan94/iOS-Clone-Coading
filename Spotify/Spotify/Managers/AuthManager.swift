3d9d3f4a9e11f486cdf91f0dbd0b9d968f82ae1f
Woochan Jeong <woochan.jeong@actionpower.kr>
2024-01-28 22:16:52 +0900

    AccessToken 갱신 메서드 추가

--- Spotify/Spotify/Managers/AuthManager.swift
+++ Spotify/Spotify/Managers/AuthManager.swift
@@ -14,15 +14,15 @@ final class AuthManager {
         static let clientID = "0c2b312ea962444287abbc42a25c08db"
         static let clientSecret = "2c3a9d6e89d14f518b2b81ecf93d27bd"
         static let tokenAPIURL = "https://accounts.spotify.com/api/token"
+        static let redirectURI = "https://www.iosacademy.io"
+        static let scopes = "user-read-private%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20user-library-modify%20user-library-read%20user-read-email"
     }
     
     private init() {}
     
     public var signInURL: URL? {
-        let scopes = "user-read-private"
-        let redirectURI = "https://www.iosacademy.io"
         let base = "https://accounts.spotify.com/authorize"
-        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(Constants.scopes)&redirect_uri=\(Constants.redirectURI)&show_dialog=TRUE"
         
         return URL(string: string)
     }
@@ -62,7 +62,7 @@ final class AuthManager {
         component.queryItems = [
             URLQueryItem(name: "grant_type", value: "authorization_code"),
             URLQueryItem(name: "code", value: code),
-            URLQueryItem(name: "redirect_uri", value: "https://www.iosacademy.io"),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
         ]
         
         var request = URLRequest(url: url)
@@ -97,7 +97,59 @@ final class AuthManager {
     
     private func cacheToken(result: AuthResponse) {
         UserDefaults.standard.setValue(result.access_token, forKey: "access_token")
-        UserDefaults.standard.setValue(result.refresh_token, forKey: "refresh_token")
+        if result.refresh_token != nil {
+            UserDefaults.standard.setValue(result.refresh_token, forKey: "refresh_token")
+        }
         UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expires_in)), forKey: "expirationDate")
+    }
+    
+    public func refreshIfNeeded(completion: @escaping (Bool) -> Void) {
+        guard shouldRefreshToken else {
+            completion(true)
+            return
+        }
+        
+        guard let refreshToken = self.refreshToken else { return }
+        
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+        
+        var component = URLComponents()
+        component.queryItems = [
+            URLQueryItem(name: "grant_type", value: "refresh_token"),
+            URLQueryItem(name: "refresh_token", value: refreshToken),
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded ", forHTTPHeaderField: "Content-Type")
+        request.httpBody = component.query?.data(using: .utf8)
+        let basicToken = Constants.clientID+":"+Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else {
+            completion(false)
+            return
+        }
+        request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                completion(false)
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                print("Result : \(result)")
+                self?.cacheToken(result: result)
+                completion(true)
+            } catch {
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+        task.resume()
+
     }
 }
--- Spotify/Spotify/Resources/AppDelegate.swift
+++ Spotify/Spotify/Resources/AppDelegate.swift
@@ -36,8 +36,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.makeKeyAndVisible()
         self.window = window
         
-        print(AuthManager.shared.signInURL?.absoluteString)
-        
         return true
     }
     
