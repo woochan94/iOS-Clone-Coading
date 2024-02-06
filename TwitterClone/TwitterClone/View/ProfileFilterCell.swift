aa5b34800c05f23c830ea0ac71bd3c917f999d5f
Woochan Jeong <vdncks25@naver.com>
2024-02-06 18:31:17 +0900

    [Twitter] Profile Tab 애니메이션 적용

---
+++ TwitterClone/TwitterClone/View/ProfileFilterCell.swift
@@ -0,0 +1,49 @@
+//
+//  ProfileFilterCell.swift
+//  TwitterClone
+//
+//  Created by Woochan Jeong on 2024/02/06.
+//
+
+import UIKit
+
+class ProfileFilterCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    private let titlelabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = .systemFont(ofSize: 14)
+        label.text = "test"
+        return label
+    }()
+    
+    override var isSelected: Bool {
+        didSet {
+            titlelabel.font = isSelected ? .boldSystemFont(ofSize: 16) : .systemFont(ofSize: 14)
+            titlelabel.textColor = isSelected ? .twitterBlue : .lightGray
+        }
+    }
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    func configureUI() {
+        backgroundColor = .white
+        
+        addSubview(titlelabel)
+        titlelabel.center(inView: self)
+    }
+}
--- TwitterClone/TwitterClone/View/ProfileHeader.swift
+++ TwitterClone/TwitterClone/View/ProfileHeader.swift
@@ -11,6 +11,8 @@ class ProfileHeader: UICollectionReusableView {
     
     // MARK: - Properties
     
+    private let filterBar = ProfileFilterView()
+    
     private lazy var containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .twitterBlue
@@ -86,12 +88,19 @@ class ProfileHeader: UICollectionReusableView {
         return stack
     }()
     
+    private let underline: UIView = {
+        let view = UIView()
+        view.backgroundColor = .twitterBlue
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         configureUI()
+        filterBar.delegate = self 
     }
     
     required init?(coder: NSCoder) {
@@ -114,6 +123,12 @@ class ProfileHeader: UICollectionReusableView {
         
         addSubview(stackView)
         stackView.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 12, paddingRight: 12)
+        
+        addSubview(filterBar)
+        filterBar.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 50)
+        
+        addSubview(underline)
+        underline.anchor(left: leftAnchor, bottom: bottomAnchor, width: frame.width / 3, height: 2)
     }
     
     // MARK: - Selector
@@ -126,4 +141,19 @@ class ProfileHeader: UICollectionReusableView {
         
     }
     
+}
+
+// MARK: - ProfileFilterViewDelegate
+
+extension ProfileHeader: ProfileFilterViewDelegate {
+    func filterView(_ view: ProfileFilterView, didSelect indexPath: IndexPath) {
+        guard let cell = view.collectionView.cellForItem(at: indexPath) as? ProfileFilterCell else {
+            return
+        }
+        
+        let xPosition = cell.frame.origin.x
+        UIView.animate(withDuration: 0.3) {
+            self.underline.frame.origin.x = xPosition
+        }
+    }
 }
